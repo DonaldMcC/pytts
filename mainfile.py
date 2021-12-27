81a7e4ce364f87c1ff3a896ad4a7cc0e951cb1b6
Donald McClymont <Donaldm2020@gmail.com>
2021-12-27 21:32:21 +0000

    support pdf and url based on filetype

--- mainfile.py
+++ mainfile.py
@@ -3,6 +3,7 @@
 #pytts test
 import pyttsx3
 import requests
+import pyPDF2
 from bs4 import BeautifulSoup
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -13,18 +14,30 @@ def speak(audio):
   engine.say(audio)
   engine.runAndWait()
 text = str(input("Paste article\n"))
-res = requests.get(text)
-soup = BeautifulSoup(res.text,'html.parser')
+if text[-3:]=='pdf':
+    reader = pyPDF2.PdfFileReader(open('story.pdf','rb'))
+    for page_num in range(pdfreader.numpages):
+        text = pdfreader.getPage(page_num).extractText()
+        cleaned_text = text.strip().replace('\n',' ')  ## Removes unnecessary spaces and break lines
+        #print(cleaned_text)                ## Print the text from PDF
+        #engine.say(cleaned_text)        ## Let The Speaker Speak The Text
+        engine.save_to_file(cleaned_text,'story.mp3')  ## Saving Text In a audio file 'story.mp3'
+        engine.runAndWait()
+else: #url
+    res = requests.get(text)
+    soup = BeautifulSoup(res.text,'html.parser')
+    articles = []
+    for i in range(len(soup.select('.p'))):
+        article = soup.select('.p')[i].getText().strip()
+        articles.append(article)
+    text = " ".join(articles)
 
-articles = []
-for i in range(len(soup.select('.p'))):
-    article = soup.select('.p')[i].getText().strip()
-    articles.append(article)
-text = " ".join(articles)
-speak(text)
-engine.save_to_file(text, 'test.mp3') ## If you want to save the speech as a audio file
-engine.runAndWait()
+    #speak(text)
+    engine.save_to_file(text, 'test.mp3') ## If you want to save the speech as a audio file
+    engine.runAndWait()
 
+
+engine.stop()
 
 ''' 
 Link To Try The Script
