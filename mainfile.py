897f3b5f6f4022f37b959a3fcabe5bb00fbf7d32
Donald McClymont <Donaldm2020@gmail.com>
2021-12-29 19:28:33 +0000

    untested but possilby working version with folder processing

--- mainfile.py
+++ mainfile.py
@@ -15,8 +15,14 @@ import requests
 import PyPDF2
 from bs4 import BeautifulSoup
 import os
-source = r'c:\users\donal\Documents\ttsimport'
-dest = r'c:\users\donal\Documents\ttsexport'
+
+#SETUP DATA - amend for your use
+source_folder = r'c:\users\donal\Documents\ttsimport'  # where you put files to be converted
+dest_folder = r'c:\users\donal\Documents\ttsexport' # where you create converted files
+
+#Not using these yet - lets see if we need to
+#lines_per_file=10  #number of lines in text or html file before creating new file
+#pages_per_file=1  #number of pages in pdf file before creating new file
 
 engine = pyttsx3.init('sapi5')  #This would need to change for non-windows as sapi is win only
 voices = engine.getProperty('voices')
@@ -42,40 +48,28 @@ def save(text, dest='story.mp3'):
   engine.runAndWait()
 
 
-f = list_files(source)
-for file in f:
-    with open(os.path.join(source,file)) as textfile:
-        extension=file[-3:]
-        if extension == 'url':
-            pass
-            #lines = textfile.readlines()
-        else:
-            callbytype(extension, sourcefile, destfile)
-engine.stop()
-
-
-def callbytype(extension, sourcefile, destfile):
+def callbytype(extension, source):
     if extension=='pdf':
-        readpdf(source, dest)
+        readpdf(source)
     elif extension=='txt':
-        readtxt(source, dest)
+        readtxt(source)
     elif extension=='url':
-        readurl(source, dest)
+        readurl(source)
     else:
         print('Extension '+ extension + ' is not supported yet')
 
 
 # below needs fixed for multiple pages
-def readpdf(file, dest):
+def readpdf(file):
     reader = PyPDF2.PdfFileReader(open(file,'rb'))
     for page_num in range(reader.numPages):
         text = reader.getPage(page_num).extractText()
         cleaned_text = text.strip().replace('\n',' ')  ## Removes unnecessary spaces and break lines
-        save(cleaned_text,dest)
+        save(cleaned_text, dest)
     return
 
 
-def readtxt(file, dest):
+def readtxt(file):
     with open(file) as fp:
         line = fp.readline()
         cnt=0
@@ -88,12 +82,31 @@ def readtxt(file, dest):
         save(story, dest)
 
 
-def readurl(file, dest)
+def readurl(file):
+    #These are assumed to be short so no file_splitting
     res = requests.get(text)
     soup = BeautifulSoup(res.text,'html.parser')
+    destname = file[:-3] + "mp3"
+    dest = os.path.join(dest_folder, destname)
     articles = []
     for i in range(len(soup.select('.p'))):
         article = soup.select('.p')[i].getText().strip()
         articles.append(article)
     text = " ".join(articles)
     save(text, dest)
+
+
+f = list_files(source)
+for file in f:
+    with open(os.path.join(source, file)) as textfile:
+        extension=file[-3:]
+        if extension == 'url':
+            with open(file) as fp:
+                line = fp.readline()
+                while line:
+                    callbytype('url', line)
+                    line = fp.readline()
+        else:
+            callbytype(extension, file)
+engine.stop()
+
